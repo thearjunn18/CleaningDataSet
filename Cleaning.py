a5b06328b4c9920c389bf1945622b1b59001faf2
thearjunn18 <thearjunn18@gmail.com>
2026-08-09 23:29:42 +0530

    Mixed Missing values

--- Cleaning.py
+++ Cleaning.py
@@ -9,3 +9,9 @@ print(object_.head())
 #checking the missing values 
 print("Missing Values in each coloume")
 print(object_.isnull().sum())
+
+#filling the missssing values
+object_["year_of_experience"].fillna(object_["year_of_experience"].mean(), implace = True)
+object_["salary"].fillna(object_["salary"].mean(), implace = True)
+object_["performance_ratio"].fillna(object_["performance_ratio"].mean(), implace = True)
+object_["year_of_experience"].fillna(object_["year_of_experience"].median(), implace = True)
