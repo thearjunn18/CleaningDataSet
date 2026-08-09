bff7f60787ce66b2ba97f79d1d6c39ec1765d76f
thearjunn18 <thearjunn18@gmail.com>
2026-08-09 23:44:26 +0530

    fixing errors

--- Cleaning.py
+++ Cleaning.py
@@ -11,14 +11,18 @@ print("Missing Values in each coloume")
 print(object_.isnull().sum())
 
 #filling the missssing values
-object_["year_of_experience"].fillna(object_["year_of_experience"].mean(), implace = True)
-object_["salary"].fillna(object_["salary"].mean(), implace = True)
-object_["performance_ratio"].fillna(object_["performance_ratio"].mean(), implace = True)
-object_["year_of_experience"].fillna(object_["year_of_experience"].median(), implace = True)
+object_["year_of_experience"].fillna(object_["year_of_experience"].mean(), inplace = True)
+object_["salary"].fillna(object_["salary"].mean(), inplace = True)
+object_["performance_ratio"].fillna(object_["performance_ratio"].mean(), inplace = True)
+object_["year_of_experience"].fillna(object_["year_of_experience"].median(), inplace = True)
 
 #filling infinite values , negative values 
 object_.replace([np.inf, -np.inf] , np.nan, inplace=True)
 object_.fillna(object_.mean(), inplace=True)
+object_["year_of_experience"].fillna(object_["year_of_experience"].mean(), inplace = True)
+object_["salary"].fillna(object_["salary"].mean(), inplace = True)
+object_["performance_ratio"].fillna(object_["performance_ratio"].mean(), inplace = True)
+object_["year_of_experience"].fillna(object_["year_of_experience"].median(), inplace = True)
 
 #creating new file 
 object_.to_csv("Cleaned_DataSet.csv", index=False)
