8a349527632555219ab98bd3c2eafa1094232919
thearjunn18 <thearjunn18@gmail.com>
2026-08-09 23:23:52 +0530

    Finding all the missing values

--- Cleaning.py
+++ Cleaning.py
@@ -6,3 +6,6 @@ import pandas as pd
 object_= pd.read_csv("/Users/arjunsharma/DatabaseCleaning/dirty_employee_dataset_30.csv")
 print(object_.head())
 
+#checking the missing values 
+print("Missing Values in each coloume")
+print(object_.isnull().sum())
